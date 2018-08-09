e3690aa4dee28ccfe924f848fdcbaa45849ccb82
Charlie Draper <charlie.draper@holition.com>
2018-08-09 13:09:39 +0100

    WORKING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import {OwlCarousel} from 'ngx-owl-carousel';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+  //   this.owlElement.trigger('play.owl.autoplay')
+  }
+  @ViewChild('owlElement') owlElement: OwlCarousel
+
   title = 'app';
+
+  images = [
+    {
+      url: '../assets/1.png',
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`
+    },
+    {
+      url: '../assets/2.png',
+      description: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.`
+    },
+    {
+      url: '../assets/3.png',
+      description: `Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.`
+    },
+    {
+      url: '../assets/4.png',
+      description: `4`
+    },
+    {
+      url: '../assets/5.png',
+      description: `5.`
+    },
+    {
+      url: '../assets/6.png',
+      description: `6.`
+    },
+    {
+      url: '../assets/7.png',
+      description: `7`
+    },
+    {
+      url: '../assets/8.png',
+      description: `8.`
+    },
+  ];
 }
